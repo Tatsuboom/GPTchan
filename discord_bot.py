3fa2f7ec1649a1973474fbeb8616c16bb4a78756
Tatsuboom <2005tatsuya0920@gmail.com>
2025-06-12 20:00:23 +0900

    conversation refactoring

--- discord_bot.py
+++ discord_bot.py
@@ -23,43 +23,33 @@ voiceclient = None
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user or len(message.mentions) >= 2 :
-        await client.process_commands(message)
-        return
-    if client.user in message.mentions and not message.mention_everyone:
-        content = message.content.replace('<@1373748611866820739>','')
-        if content == None or len(content) <= 1:
-            await message.reply("呼んだ？")
-            await client.process_commands(message)
-            return
-        
-        async with message.channel.typing():
-            output_text = gpt_api.createTextResponse(content)
-            if output_text:
-                if voiceclient != None and voiceclient.is_connected():
-                    voicevox_api.createvoice(output_text)
-                    voiceclient.play(discord.FFmpegPCMAudio("output.wav"))
-
-                await message.reply(output_text)
-    await client.process_commands(message)
-
-#応答生成(コマンド版)
-@client.command()
-async def talk(ctx,*, message: str = None):
-    if message == None or len(message) <= 1:
-        await ctx.channel.send("呼んだ？")
+#会話生成
+async def conversation(text):
+    if text.content == None or len(text.content) <= 1:
+        await text.channel.send("呼んだ？")
         return
         
-    async with ctx.typing():
-        output_text = gpt_api.createTextResponse(message)
+    async with text.channel.typing():
+        output_text = gpt_api.createTextResponse(text.content)
         if output_text:
             if voiceclient != None and voiceclient.is_connected():
                 voicevox_api.createvoice(output_text)
                 voiceclient.play(discord.FFmpegPCMAudio("output.wav"))
 
-            await ctx.channel.send(output_text)
+            await text.channel.send(output_text)
+
+@client.event
+async def on_message(message):
+    if client.user in message.mentions and not message.mention_everyone:
+        message.content = message.content.replace('<@1373748611866820739>','')
+        await conversation(message)
+    await client.process_commands(message)
+
+@client.command()
+async def talk(ctx):
+    ctx.message.content = ctx.message.content.replace('!talk','')
+    await conversation(ctx.message)
+
 
 #ボイスチャンネル接続
 @client.command()
